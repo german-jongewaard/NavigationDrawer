bacaeb05909af33722bf5d4cee94d36a2a7ad0d3
german <germanjongewaard@gmail.com>
2018-01-08 02:12:59 +0100

    toolbar

--- app/src/main/java/dev/com/jongewaard/navigationdrawer/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/navigationdrawer/MainActivity.java
@@ -2,6 +2,8 @@ package dev.com.jongewaard.navigationdrawer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setToolBar(){
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+
+
     }
 }
